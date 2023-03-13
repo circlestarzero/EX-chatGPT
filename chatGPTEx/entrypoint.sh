caf6380da0bd069d5f02070c7cebcf79d3cd41ed
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-03-13 14:38:23 +0800

    Update entrypoint.sh

--- chatGPTEx/entrypoint.sh
+++ chatGPTEx/entrypoint.sh
@@ -32,7 +32,7 @@ else
     echo "更新失败，继续使用旧的程序来启动..."
 fi
 
-sed -i 's/app.run(host="127\.0\.0\.1",port=1234)/#app.run(host="127\.0\.0\.1",port=1234)/g; s/# app.run(host="0\.0\.0\.0", port = 5000)/app.run(host="0\.0\.0\.0", port = 5000)/g' main.py
+sed -i 's/app.run(host="127\.0\.0\.1",port=1234)/#app.run(host="127\.0\.0\.1",port=1234)/g; s/# app.run(host="0\.0\.0\.0", port = 5000)/app.run(host="0\.0\.0\.0", port = 5000)/g' /app/chatGPTEx/main.py
 
 # 启动主程序
 exec python /app/chatGPTEx/main.py
