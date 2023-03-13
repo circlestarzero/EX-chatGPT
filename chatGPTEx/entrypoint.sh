fa2c0c5e22f8c1a8406664f62159b1dfa26a0b6f
唯依 <46625336+onlyLTY@users.noreply.github.com>
2023-03-13 15:22:47 +0800

    Update entrypoint.sh

--- chatGPTEx/entrypoint.sh
+++ chatGPTEx/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd ${WORKDIR}
 
-sed -i 's/app.run(host="127\.0\.0\.1",port=1234)/#app.run(host="127\.0\.0\.1",port=1234)/g; s/# app.run(host="0\.0\.0\.0", port = 5000)/app.run(host="0\.0\.0\.0", port = 5000)/g' /app/chatGPTEx/main.py
-
 # 启动主程序
 exec python /app/chatGPTEx/main.py
